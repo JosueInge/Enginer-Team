9ed39dcb522cfc83649b2ef1f51c03b77aa2878c
JosueInge <wilfredojosue438@gmail.com>
2025-09-13 21:23:18 -0600

    Alineacion de botones a la izquierda de publicar y enviar

--- turismo.php
+++ turismo.php
@@ -178,7 +178,7 @@ echo "<pre>ROL ACTUAL: " . $_SESSION['usuario_rol'] . "</pre>";
     }
     .boton-publicar { 
       position: fixed; 
-      bottom: 30px; right: 
+      bottom: 30px; lefte: 
       30px; background-color: #0d5c9b; 
       color: white; 
       border: none; 
