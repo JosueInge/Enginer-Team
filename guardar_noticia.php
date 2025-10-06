215bfe402d7d9451e341e9c064a4626e2ab95c21
JosueInge <wilfredojosue438@gmail.com>
2025-10-05 21:23:54 -0600

    Correcion de redirecciones para usuarios invitados y correcion en menu lateral de categoria

--- guardar_noticia.php
+++
@@ -1,2 +0,0 @@
-// Cambiar todas las referencias de 'password' a 'contraseña'
-$stmt = $conexion->prepare("UPDATE usuarios SET nombre = ?, contraseña = ? WHERE id = ?");
--- test_final.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config.php';
-
-echo "Client ID: " . (MICROSOFT_CLIENT_ID ?: 'VACÍO') . "<br>";
-echo "Client Secret: " . (MICROSOFT_CLIENT_SECRET ?: 'VACÍO') . "<br>";
-echo "Redirect URI: " . MICROSOFT_REDIRECT_URI . "<br>";
-?>
